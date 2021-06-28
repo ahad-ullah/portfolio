7e4ffcf0a1aad4602ec3459be0e87acf99569868
ahad-ullah <74977782+ahad-ullah@users.noreply.github.com>
2021-06-28 17:04:10 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def message():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+        return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
