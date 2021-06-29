2f4b57b5752347458d4e4bae8eee62cfb3410669
Ahad Ullah <ahadullahkhokhar@gmail.com>
2021-06-29 09:29:43 +0500

    final touch

--- app.py
+++ app.py
@@ -11,11 +11,8 @@ def index():
 @app.route('/', methods=['POST'])
 def message():
     if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        message = request.form['message']
         return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True)
